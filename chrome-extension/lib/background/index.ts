ed2900dcef25a4093b100c7fd89f33b88518c166
Lorenzo <lorenzo@Lorenzos-MacBook-Air.local>
2024-10-06 11:02:36 -0400

    stuff

--- chrome-extension/lib/background/index.ts
+++ chrome-extension/lib/background/index.ts
@@ -1,4 +1,14 @@
 import 'webextension-polyfill';
 
-console.log('background loaded');
-console.log("Edit 'chrome-extension/lib/background/index.ts' and save to reload.");
+window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', e => {
+  const iconVariant = e.matches ? '-white' : '';
+
+  chrome.browserAction.setIcon({
+    path: {
+      16: `icon${iconVariant}-16.png`,
+      32: `icon${iconVariant}-32.png`,
+      48: `icon${iconVariant}-48.png`,
+      128: `icon${iconVariant}-128.png`,
+    },
+  });
+});
